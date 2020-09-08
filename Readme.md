479eb01e01c0df610acabbea5b06ae23a65744d5
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2020-09-08 19:32:08 +0200

    Solve bugs

---
+++ Readme.md
@@ -0,0 +1,19 @@
+# cpy-t
+
+### Copytool
+
+## DEPS
+
+Before you use it you need `Node.Js` and `NPM` (with `npx`)
+
+## Online Use
+```npx cpy-t fileOrDir targetfileOrDir```
+
+## Offline
+
+### Install (NEED INTERNET)
+
+`npm i -g cpy-t`
+
+### Use
+`cpy-t fileordir targetfileordir`
--- cpdir.js
+++ cpdir.js
@@ -1,28 +1,52 @@
 #!/usr/bin/env node
+(()=>{
+const cpt =["CCCCCCCCCCCCCCC",
+			"CCCCCCCCCCCCCCC"
+			"CCCCCC",
+			"CCCC",
+			"CC",
+			"CCCC",
+			"CCCCCC",
+			"CCCCCCCCCCCCCCC",
+			"CCCCCCCCCCCCCCC"];
+for(let vge of cpt){
+	console.log(vge);/*ICON*/
+}
 console.log(`The Copy Tool\n<c>Sharkbyteprojects`);
 const paths   = require("path");
 const fs     = require("fs");
-function cpydir(dir, target, fis){
+function cpydir(dir, target){
     try{
         const stati=fs.statSync(dir);
         if(stati.isDirectory()){
-            if(!fis){
+			process.stdout.write(`Copy Dir "${dir}" to "${target}"`);
             const bgd=paths.parse(paths.resolve(dir)).base;
-            console.log(paths.resolve(target));
-            fs.mkdirSync(paths.resolve(target));
-            }
+			const tgadir=paths.resolve(target);
+			if(fs.existsSync(tgadir)){
+            fs.mkdirSync(tgadir);//MAKE NEW DIR
+			}
             const dirs = fs.readdirSync(dir);
             for(let cur of dirs){
-                cpydir(paths.resolve(dir, cur), paths.resolve(target, cur), false);
+                cpydir(paths.resolve(dir, cur), paths.resolve(target, cur));
             }
         }else{
+			process.stdout.write(`Copy File "${dir}" to "${target}"`);
             fs.copyFileSync(dir,target);
         }
+		process.stdout.write(`...Complete\n"`);
     }catch(e){
         console.error(`ERR in ${dir} to ${target}`);
     }
 }
+const ot=process.title;
+process.title="cpy-t";
 const dir=process.argv[2];
 const target=process.argv[3];
-cpydir(dir, target, true);
-console.log("-----------------------\nCOMPLETE");
+if(dir&&target){
+	cpydir(dir, target);
+	console.log("-----------------------\nCOMPLETE, ALL FILES WHERE COPY");
+}else{
+	console.error("usage:\n\tcpy dir target\n\t target must be empty");
+}
+process.title=ot;
+})();
